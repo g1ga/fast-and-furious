c3da3bac8bceb81183e8df57ac5c0d3fd3b2bba0
Luigi Bellanca <luigi.bellanca@gmail.com>
2013-06-30 20:03:29 +0200

    added relay command stuff
    wpf, mvvm

---
+++ fast-and-furious/BasicRelayCommand/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using MvvmCommands.Annotations;
+
+namespace MvvmCommands
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private int _counter;
+
+        public MainWindowViewModel()
+        {
+            IncrementCommand = new RelayCommand(() => Counter++, () => _counter < 10);
+            DecrementCommand = new RelayCommand(() => Counter--, () => _counter > -5);
+        }
+
+        public IRelayCommand IncrementCommand { get; private set; }
+        public IRelayCommand DecrementCommand { get; private set; }
+
+        public int Counter
+        {
+            get { return _counter; }
+            set
+            {
+                _counter = value;
+                OnPropertyChanged("Counter");
+                IncrementCommand.RaiseCanExecuteChanged();
+                DecrementCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ fast-and-furious/BasicRelayCommand/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MvvmCommands
+{
+    public interface IRelayCommand
+    {
+        void Execute(object parameter);
+        bool CanExecute(object parameter);
+        event EventHandler CanExecuteChanged;
+        void RaiseCanExecuteChanged();
+    }
+
+    class RelayCommand : IRelayCommand, ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+    
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null) 
+                handler(this, EventArgs.Empty);
+        }
+    }
+}
