cc8be89acbdaca76246ad130893110ba40931713
Luigi Bellanca <luigi.bellanca@gmail.com>
2013-07-02 22:38:06 +0200

    avalondock demo

---
+++ DockManager/DockManager/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Xceed.Wpf.AvalonDock.Themes;
+
+namespace DockManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //DockingManager.Theme = new MetroTheme();
+        }
+    }
+}
